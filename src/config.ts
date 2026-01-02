fc4bff3da8046c18510a35ce621f426292d520f5
niwaniwa <10182706+niwaniwa@users.noreply.github.com>
2026-01-02 23:47:46 +0900

    update: ソーシャルリンクとmvvについてconfigに移設

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,26 @@
 export const BLOG_PAGE_SIZE = 10;
+
+// サイト情報
+export const SITE = {
+  domain: "niri.la",
+  name: "nirila",
+  description: "engineer, creator",
+};
+
+// MVV
+export const MVV = {
+  mission: "VRChat",
+  vision: "Resonite",
+  values: ["GO", "LetGO", "GOOOO"],
+};
+
+// SNSリンク
+export const SOCIAL_LINKS = [
+  { id: "github", url: "https://github.com/niwaniwa" },
+  { id: "twitter", url: "https://x.com/ni_rilana" },
+  {
+    id: "misskey",
+    url: "https://misskey.niri.la/@ni_rilana",
+    label: "Misskey",
+  },
+];
